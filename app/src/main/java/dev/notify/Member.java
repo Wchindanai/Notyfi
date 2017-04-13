2a10a0cdcc49c17dc45dfeba6d40e1efdbbe0722
Chindanai Wasanachin <dn.chindanai@gmail.com>
2017-04-14 04:38:37 +0700

    Create Sign Out Button

--- app/src/main/java/dev/notify/Member.java
+++ app/src/main/java/dev/notify/Member.java
@@ -1,16 +1,21 @@
 package dev.notify;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Member extends AppCompatActivity {
+    private static final String TAG = "Member";
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
 
@@ -37,7 +42,27 @@ public class Member extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.member_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.sign_out:
+                Log.d(TAG, "onOptionsItemSelected: OnClick");
+                SharedPreferences sharedPreferences = getSharedPreferences("notify", 0);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.commit();
+                startActivity(new Intent(getApplicationContext(), Login.class));
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     private void getData() {
 
